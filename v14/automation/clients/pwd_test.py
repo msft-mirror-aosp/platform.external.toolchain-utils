2f1af985d8844d8c3cd135a5b75a6472d3b34c21
asharif <asharif@p4.invalid>
2013-02-15 05:20:48 +0000

    Added a simple test for printing the working directory.  The update_job uses the pwd to track the current branch in git.  This test makes sure changes to command_executer will not break  the update_job.
    PRESUBMIT=passed
    R=raymes,bjanakiraman
    DELTA=18  (18 added, 0 deleted, 0 changed)
    OCL=46557-p2
    RCL=46560-p2
    RDATE=2011/01/18 13:23:23
    
    
    P4 change: 42608759

---
+++ v14/automation/clients/pwd_test.py
@@ -0,0 +1,18 @@
+import xmlrpclib
+from utils import utils
+from automation.common import job
+from automation.common import job_group
+
+server = xmlrpclib.Server("http://localhost:8000")
+
+command = "echo These following 3 lines should be the same"
+command += "&& pwd"
+command += "&& echo $(pwd)"
+command += "&& echo ${PWD}"
+
+pwd_job = job.Job(command)
+pwd_job.AddRequiredMachine("", "linux", False)
+
+job_group = job_group.JobGroup([pwd_job])
+ids = server.ExecuteJobGroup(utils.Serialize(job_group))
+
