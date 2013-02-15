56d78bc805deb158c595fcb81a605ac3b25dc9aa
asharif <asharif@p4.invalid>
2013-02-15 21:15:08 +0000

    Added ability to pull sources from git repositories.
    Unified a few functions and refactored the file.
    
    PRESUBMIT=passed
    R=cmtice,raymes,bjanakiraman,kbaclawski
    APPROVED=cmtice
    DELTA=182  (89 added, 30 deleted, 63 changed)
    OCL=55171-p2
    RCL=55215-p2
    RDATE=2011/08/24 13:49:30
    
    
    P4 change: 42652798

--- v14/repo_to_repo.py
+++ v14/repo_to_repo.py
@@ -28,6 +28,14 @@ def GetCanonicalMappings(mappings):
     canonical_mappings.append(perforce.PathMapping(remote_path, local_path))
   return canonical_mappings
 
+def SplitMapping(mapping):
+  if " " in mapping:
+    remote_path, local_path = mapping.split()
+  else:
+    local_path = "."
+    remote_path = mapping
+  return remote_path, local_path
+
 
 class Repo(object):
   def __init__(self):
@@ -39,11 +47,37 @@ class Repo(object):
     self.ignores.append(".gitignore")
     self.ignores.append(".p4config")
     self.ignores.append("README.google")
+    self._root_dir = tempfile.mkdtemp()
+    self._ce = command_executer.GetCommandExecuter()
+    self._logger = logger.GetLogger()
 
-
-  def PullSources(self, root_dir):
+  def PullSources(self):
+    """ Pull all sources into an internal dir."""
     pass
 
+  def SetupForPush(self):
+    """ Setup a repository for pushing later."""
+    pass
+
+  def PushSources(self, commit_message, dry_run=False):
+    """ Push to the external repo with the commit message."""
+    pass
+
+  def _RsyncExcludingRepoDirs(self, source_dir, dest_dir):
+    command = "rsync -a --exclude=.git --exclude=.svn %s/ %s/" % (source_dir,
+                                                               dest_dir)
+    return self._ce.RunCommand(command)
+
+  def MapSources(self, dest_dir):
+    """Copy sources from the internal dir to root_dir."""
+    return self._RsyncExcludingRepoDirs(self._root_dir, dest_dir)
+
+  def GetRoot(self):
+    return self._root_dir
+
+  def CleanupRoot(self):
+    command = "rm -rf %s" % self._root_dir
+    return self._ce.RunCommand(command)
 
   def __str__(self):
     return "\n".join(str(s) for s in [self.repo_type,
@@ -58,27 +92,23 @@ class P4Repo(Repo):
     self.address = address
     self.mappings = mappings
 
-
-  def PullSources(self, root_dir):
-    ce = command_executer.GetCommandExecuter()
-
+  def PullSources(self):
     client_name = socket.gethostname()
     client_name += tempfile.mkstemp()[1].replace("/", "-")
     mappings = self.mappings
     port = self.address
-    checkout_dir = root_dir
     p4view = perforce.View("depot2",
                            GetCanonicalMappings(mappings))
-    p4client = perforce.CommandsFactory(checkout_dir, p4view, name=client_name)
+    p4client = perforce.CommandsFactory(self._root_dir, p4view, name=client_name)
     command = p4client.SetupAndDo(p4client.Sync())
-    ret = ce.RunCommand(str(command))
+    ret = self._ce.RunCommand(str(command))
     assert ret == 0, "Could not setup client."
     command = p4client.InCheckoutDir(p4client.SaveCurrentCLNumber())
-    ret, o, e = ce.RunCommand(str(command), return_output=True)
+    ret, o, e = self._ce.RunCommand(str(command), return_output=True)
     assert ret == 0, "Could not get version from client."
     self.revision = re.search("^\d+$", o.strip(), re.MULTILINE).group(0)
     command = p4client.InCheckoutDir(p4client.Remove())
-    ret = ce.RunCommand(str(command))
+    ret = self._ce.RunCommand(str(command))
     assert ret == 0, "Could not delete client."
     return 0
 
@@ -90,38 +120,26 @@ class SvnRepo(Repo):
     self.address = address
     self.mappings = mappings
 
-
-  def PullSources(self, root_dir):
-    ce = command_executer.GetCommandExecuter()
-
-    command = "mkdir -p %s && cd %s" % (root_dir, root_dir)
-    for mapping in self.mappings:
-      if " " in mapping:
-        remote_path, local_path = mapping.split()
-      else:
-        local_path = "."
-        remote_path = mapping
-      command += "&& svn co %s/%s %s" % (self.address, remote_path, local_path)
-    ret = ce.RunCommand(command)
-    if ret: return ret
-
-    command = "cd %s" % (root_dir)
-    self.revision = ""
-    for mapping in self.mappings:
-      if " " in mapping:
-        remote_path, local_path = mapping.split()
-      else:
-        local_path = "."
-        remote_path = mapping
-      command += "&& cd %s && svnversion ." % (local_path)
-      ret, o, e = ce.RunCommand(command, return_output=True)
-      self.revision += o.strip()
+  def PullSources(self):
+    with utils.CWDSwitcher(self._root_dir):
+      for mapping in self.mappings:
+        remote_path, local_path = SplitMapping(mapping)
+        command += "svn co %s/%s %s" % (self.address, remote_path, local_path)
+      ret = self._ce.RunCommand(command)
       if ret: return ret
+
+      self.revision = ""
+      for mapping in self.mappings:
+        remote_path, local_path = SplitMapping(mapping)
+        command = "cd %s && svnversion ." % (local_path)
+        ret, o, e = self._ce.RunCommand(command, return_output=True)
+        self.revision += o.strip()
+        if ret: return ret
     return 0
 
 
 class GitRepo(Repo):
-  def __init__(self, address, branch, ignores=None):
+  def __init__(self, address, branch, mappings=None, ignores=None):
     Repo.__init__(self)
     self.repo_type = "git"
     self.address = address
@@ -131,48 +149,76 @@ class GitRepo(Repo):
       self.branch = branch
     if ignores:
       self.ignores += ignores
+    self.mappings = mappings
 
+  def _CloneSources(self):
+    with utils.CWDSwitcher(self._root_dir):
+      command = "git clone %s ." % (self.address)
+      return self._ce.RunCommand(command)
 
-  def SetupForPush(self, root_dir):
-    ce = command_executer.GetCommandExecuter()
-    command = "mkdir -p %s && cd %s" % (root_dir, root_dir)
-    command += "&& git clone -v %s ." % self.address
-    ret = ce.RunCommand(command)
-    logger.GetLogger().LogFatalIf(ret, "Could not clone git repo %s." %
-                                  self.address)
+  def PullSources(self):
+    with utils.CWDSwitcher(self._root_dir):
+      ret = self._CloneRepo()
+      if ret: return ret
 
-    command = "cd %s" % root_dir
-    command += "&& git branch -a | grep -wq %s" % self.branch
-    ret = ce.RunCommand(command)
+      command = "git checkout %s" % self.branch
+      ret = self._ce.RunCommand(command)
+      if ret: return ret
 
-    command = "cd %s" % root_dir
-    if ret == 0:
-      if self.branch != "master":
-        command += ("&& git branch --track %s remotes/origin/%s" %
-                    (self.branch, self.branch))
-      command += "&& git checkout %s" % self.branch
-    else:
-      command += "&& git symbolic-ref HEAD refs/heads/%s" % self.branch
-    command += "&& rm -rf *"
-    ret = ce.RunCommand(command)
-    return ret
+      command = "git describe --always"
+      ret, o, e = self._ce.RunCommand(command, return_output=True)
+      self.revision = o.strip()
+      return ret
 
+  def SetupForPush(self):
+    with utils.CWDSwitcher(self._root_dir):
+      ret = self._CloneSources()
+      logger.GetLogger().LogFatalIf(ret, "Could not clone git repo %s." %
+                                    self.address)
 
-  def PushSources(self, root_dir, commit_message, dry_run=False):
-    ce = command_executer.GetCommandExecuter()
-    push_args = ""
-    if dry_run:
-      push_args += " -n "
+      command = "git branch -a | grep -wq %s" % self.branch
+      ret = self._ce.RunCommand(command)
 
-    command = "cd %s" % (root_dir)
-    if self.ignores:
-      for ignore in self.ignores:
-        command += "&& echo \"%s\" >> .git/info/exclude" % ignore
-    command += "&& git add -Av ."
-    command += "&& git commit -v -m \"%s\"" % commit_message
-    command += "; git push -v %s origin %s:%s" % (push_args, self.branch, self.branch)
-    ret = ce.RunCommand(command)
-    return ret
+      if ret == 0:
+        if self.branch != "master":
+          command = ("git branch --track %s remotes/origin/%s" %
+                      (self.branch, self.branch))
+        else:
+          command = "pwd"
+        command += "&& git checkout %s" % self.branch
+      else:
+        command = "git symbolic-ref HEAD refs/heads/%s" % self.branch
+      command += "&& rm -rf *"
+      ret = self._ce.RunCommand(command)
+      return ret
+
+  def PushSources(self, commit_message, dry_run=False):
+    with utils.CWDSwitcher(self._root_dir):
+      push_args = ""
+      if dry_run:
+        push_args += " -n "
+
+      command = "pwd"
+      if self.ignores:
+        for ignore in self.ignores:
+          command += "&& echo \"%s\" >> .git/info/exclude" % ignore
+      command += "&& git add -Av ."
+      command += "&& git commit -v -m \"%s\"" % commit_message
+      command += "; git push -v %s origin %s:%s" % (push_args, self.branch, self.branch)
+      return self._ce.RunCommand(command)
+
+  def MapSources(self, root_dir):
+    if not self.mappings:
+      return
+    with utils.CWDSwitcher(self._root_dir):
+      for mapping in self.mappings:
+        remote_path, local_path = SplitMapping(mapping)
+        remote_path.rstrip("...")
+        local_path.rstrip("...")
+        ret = self._RsyncExcludingRepoDirs(remote_path,
+                                               os.path.join(root_dir, local_path))
+        if ret: return ret
+    return 0
 
 
 class RepoReader():
@@ -182,14 +228,12 @@ class RepoReader():
     self.input_repos = []
     self.output_repos = []
 
-
   def ParseFile(self):
     f = open(self.filename)
     self.main_dict = eval(f.read())
     self.CreateReposFromDict(self.main_dict)
     f.close()
     return [self.input_repos, self.output_repos]
-
 
   def CreateReposFromDict(self, main_dict):
     for key, val in main_dict.items():
@@ -203,13 +247,11 @@ class RepoReader():
         else:
           logger.GetLogger().LogFatal("Unknown key: %s found" % key)
 
-
   def GetDictValue(self, dictionary, key):
     if key in dictionary:
       return dictionary[key]
     else:
       return None
-
 
   def CreateRepoFromDict(self, repo_dict):
     repo_type = self.GetDictValue(repo_dict, "type")
@@ -227,6 +269,7 @@ class RepoReader():
     elif repo_type == "git":
       repo = GitRepo(repo_address,
                      repo_branch,
+                     mappings=repo_mappings,
                      ignores=repo_ignores)
     else:
       logger.GetLogger().LogFatal("Unknown repo type: %s" % repo_type)
@@ -235,8 +278,6 @@ class RepoReader():
 
 @logger.HandleUncaughtExceptions
 def Main(argv):
-  root_dir = tempfile.mkdtemp()
-
   parser = optparse.OptionParser()
   parser.add_option("-i",
                     "--input_file",
@@ -258,24 +299,29 @@ def Main(argv):
   [input_repos, output_repos] = rr.ParseFile()
 
   for output_repo in output_repos:
-    ret = output_repo.SetupForPush(root_dir)
+    ret = output_repo.SetupForPush()
     if ret: return ret
 
   input_revisions = []
   for input_repo in input_repos:
-    ret = input_repo.PullSources(root_dir)
+    ret = input_repo.PullSources()
     if ret: return ret
     input_revisions.append(input_repo.revision)
 
+  for input_repo in input_repos:
+    for output_repo in output_repos:
+      ret = input_repo.MapSources(output_repo.GetRoot())
+      if ret: return ret
+
   commit_message = "Sync'd repos to: %s" % ",".join(input_revisions)
   for output_repo in output_repos:
-    ret = output_repo.PushSources(root_dir, commit_message, dry_run=options.dry_run)
+    ret = output_repo.PushSources(commit_message, dry_run=options.dry_run)
     if ret: return ret
 
   if not options.dry_run:
-    ce = command_executer.GetCommandExecuter()
-    command = "rm -rf %s" % root_dir
-    ret = ce.RunCommand(command)
+    for output_repo in output_repos:
+      output_repo.CleanupRoot()
+    
 
   return ret
 
