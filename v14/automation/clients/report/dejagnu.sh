a84c0e89301bacb936c8a9f926e13daf8e911d67
kbaclawski <kbaclawski@p4.invalid>
2013-02-15 21:35:45 +0000

    DejaGNU reporting tool: don't assume that the database file is in a fixed location.
    Tested:
      Locally.
    
    PRESUBMIT=passed
    R=jingyu,shenhan
    CC=bhaskar-staff
    DELTA=4  (3 added, 0 deleted, 1 changed)
    OCL=55678-p2
    RCL=55682-p2
    RDATE=2011/09/21 14:42:18
    
    
    P4 change: 42654571

--- v14/automation/clients/report/dejagnu.sh
+++ v14/automation/clients/report/dejagnu.sh
@@ -7,7 +7,10 @@
 export PYTHONPATH="$(pwd)"
 export DJANGO_SETTINGS_MODULE="dejagnu.settings"
 
-if [ ! -f "dejagnu.db" ]; then
+declare -r DATABASE_FILE=$(python -c \
+  "import $DJANGO_SETTINGS_MODULE;print $DJANGO_SETTINGS_MODULE.DATABASE_NAME")
+
+if [ ! -f "$DATABASE_FILE" ]; then
   django-admin sqlall dejagnu
   django-admin syncdb
 fi
