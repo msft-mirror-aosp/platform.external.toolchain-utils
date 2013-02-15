3af6d1173a623662d256a696216337d6646759b5
asharif <asharif@p4.invalid>
2013-02-15 05:15:45 +0000

    Modified command executer so that killing local jobs would  kill remote jobs.
    PRESUBMIT=passed
    R=raymes,bjanakiraman
    DELTA=42  (41 added, 0 deleted, 1 changed)
    OCL=46388-p2
    RCL=46390-p2
    RDATE=2011/01/11 18:11:29
    
    
    P4 change: 42608491

---
+++ v14/remote_kill_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Script to wrap run_remote_tests.sh script.
+
+Run this script and kill it. Then run ps -ef to see if sleep
+is still running,. 
+"""
+
+__author__ = "asharif@google.com (Ahmad Sharif)"
+
+import optparse
+import os
+import re
+import sys
+import subprocess
+from utils import command_executer
+from utils import utils
+
+
+def Usage(parser, message):
+  print "ERROR: " + message
+  parser.print_help()
+  sys.exit(0)
+
+def Main(argv):
+  parser = optparse.OptionParser()
+  parser.add_option("-c", "--chromeos_root", dest="chromeos_root",
+                    help="ChromeOS root checkout directory")
+  parser.add_option("-r", "--remote", dest="remote",
+                    help="Remote chromeos device.")
+  options = parser.parse_args(argv)[0]
+  ce = command_executer.GetCommandExecuter()
+  ce.RunCommand("sleep 10000",
+                machine=os.uname()[1])
+  return 0
+
+
+if __name__ == "__main__":
+  Main(sys.argv)
