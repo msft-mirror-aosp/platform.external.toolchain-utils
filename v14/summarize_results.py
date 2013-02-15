f12f79d7f770a514ac3e46d8741aef64eda0bc67
raymes <raymes@p4.invalid>
2013-02-15 05:16:01 +0000

    Added results summarizer.
    PRESUBMIT=passed
    R=asharif,bjanakiraman
    DELTA=78  (78 added, 0 deleted, 0 changed)
    OCL=46472-p2
    RCL=46476-p2
    RDATE=2011/01/14 11:23:52
    
    
    P4 change: 42608591

---
+++ v14/summarize_results.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Script to summarize the results of various log files."""
+
+__author__ = "raymes@google.com (Raymes Khoury)"
+
+import sys
+
+class DejaGNUSummarizer:
+  def Matches(self, log_file):
+    for log_line in log_file:
+      if log_line.find("""tests ===""") > -1:
+        return True
+    return False
+
+  def Summarize(self, log_file):
+    result = ""
+    pass_statuses = ["PASS", "XPASS"]
+    fail_statuses = ["FAIL", "XFAIL", "UNSUPPORTED", "ERROR", "WARNING"]
+    for line in log_file:
+      line = line.strip().split(":")
+      if len(line) > 1 and (line[0] in pass_statuses or
+                            line[0] in fail_statuses):
+        test_name = (":".join(line[1:])).replace("\t", " ").strip()
+        if line[0] in pass_statuses:
+          test_result = "pass"
+        else:
+          test_result = "fail"
+        result += "%s\t%s\n" % (test_name, test_result)
+    return result
+
+class AutoTestSummarizer:
+  def Matches(self, log_file):
+    for log_line in log_file:
+      if log_line.find("""Installing autotest on""") > -1:
+        return True
+    return False
+
+  def Summarize(self, log_file):
+    result = ""
+    pass_statuses = ["PASS"]
+    fail_statuses = ["FAIL"]
+    for line in log_file:
+      line = line.strip().split(" ")
+      if len(line) > 1 and (line[-1].strip() in pass_statuses or
+                            line[-1].strip() in fail_statuses):
+        test_name = (line[0].strip())
+        if line[-1].strip() in pass_statuses:
+          test_result = "pass"
+        else:
+          test_result = "fail"
+        result += "%s\t%s\n" % (test_name, test_result)
+    return result
+
+def Usage():
+  print "Usage: %s log_file" % sys.argv[0]
+  sys.exit(1)
+
+def Main(argv):
+  if len(argv) != 2:
+    Usage()
+  filename = argv[1]
+
+  summarizers = [DejaGNUSummarizer(), AutoTestSummarizer()]
+  f = open(filename, 'rb')
+  for summarizer in summarizers:
+    f.seek(0)
+    if summarizer.Matches(f):
+      f.seek(0)
+      print summarizer.Summarize(f)
+      exit(0)
+  print "Log file not in expected format"
+
+if __name__ == "__main__":
+  Main(sys.argv)
+
