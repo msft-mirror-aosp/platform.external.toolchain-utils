bb275f3117f49fea6eb79baa166aaa14f30249b7
kbaclawski <kbaclawski@p4.invalid>
2013-02-15 20:43:01 +0000

    Add another shell command container used to contruct shell pipelines. Extract the common part of Chain and Pipe to AbstractCommandContainer.
    Tested:
      Dry run of nightly.py and crosstool.py clients.
    
    PRESUBMIT=passed
    R=asharif,raymes
    CC=bjanakiraman
    DELTA=76  (61 added, 8 deleted, 7 changed)
    OCL=54370-p2
    RCL=54392-p2
    RDATE=2011/07/12 14:33:55
    
    
    P4 change: 42650141

--- v14/automation/common/command.py
+++ v14/automation/common/command.py
@@ -4,6 +4,7 @@
 
 __author__ = 'kbaclawski@google.com (Krystian Baclawski)'
 
+import abc
 import collections
 import os.path
 
@@ -14,7 +15,7 @@ class Shell(object):
   def __init__(self, cmd, *args, **kwargs):
     assert all(key in ['path'] for key in kwargs)
 
-    self._cmd  = cmd
+    self._cmd = cmd
     self._args = list(args)
     self._path = kwargs.get('path', '')
 
@@ -51,19 +52,11 @@ class Wrapper(object):
     return str(Chain(self._prefix, self._command, self._suffix))
 
 
-class Chain(collections.MutableSequence):
-  """Container that chains shell commands using && operator."""
+class AbstractCommandContainer(collections.MutableSequence):
+  """Common base for all classes that behave like command container."""
 
   def __init__(self, *commands):
-    self._commands = []
-    self._commands.extend(commands)
-
-  @staticmethod
-  def _ValidateType(command):
-    if type(command) not in [str, Shell, Chain]:
-      raise TypeError('Command cannot have %s type.' % type(command))
-    else:
-      return command
+    self._commands = list(commands)
 
   def __contains__(self, command):
     return command in self._commands
@@ -78,13 +71,73 @@ class Chain(collections.MutableSequence):
     return self._commands[index]
 
   def __setitem__(self, index, command):
-    self._commands[index] = self._ValidateType(command)
+    self._commands[index] = self._ValidateCommandType(command)
 
   def __delitem__(self, index):
     del self._commands[index]
 
   def insert(self, index, command):
-    self._commands.insert(index, self._ValidateType(command))
+    self._commands.insert(index, self._ValidateCommandType(command))
+
+  @abc.abstractmethod
+  def __str__(self):
+    pass
+
+  @abc.abstractproperty
+  def stored_types(self):
+    pass
+
+  def _ValidateCommandType(self, command):
+    if type(command) not in self.stored_types:
+      raise TypeError('Command cannot have %s type.' % type(command))
+    else:
+      return command
+
+  def _StringifyCommands(self):
+    cmds = []
+
+    for cmd in self:
+      if type(cmd) > AbstractCommandContainer and len(cmd) > 1:
+        cmds.append('(%s)' % cmd)
+      else:
+        cmds.append(str(cmd))
+
+    return cmds
+
+
+class Chain(AbstractCommandContainer):
+  """Container that chains shell commands using (&&) shell operator."""
+
+  @property
+  def stored_types(self):
+    return [str, Shell, Chain, Pipe]
 
   def __str__(self):
-    return ' && '.join(str(cmd) for cmd in self)
+    return ' && '.join(self._StringifyCommands())
+
+
+class Pipe(AbstractCommandContainer):
+  """Container that chains shell commands using pipe (|) operator."""
+
+  def __init__(self, *commands, **kwargs):
+    assert all(key in ['input', 'output'] for key in kwargs)
+
+    AbstractCommandContainer.__init__(self, *commands)
+
+    self._input = kwargs.get('input', None)
+    self._output = kwargs.get('output', None)
+
+  @property
+  def stored_types(self):
+    return [str, Shell]
+
+  def __str__(self):
+    pipe = self._StringifyCommands()
+
+    if self._input:
+      pipe.insert(str(Shell('cat', self._input), 0))
+
+    if self._output:
+      pipe.append(str(Shell('tee', self._output)))
+
+    return ' | '.join(pipe)
