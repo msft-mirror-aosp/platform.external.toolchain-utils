8c227da7a8233b153db8954adbb749f8093f79a8
asharif <asharif@p4.invalid>
2013-02-15 04:35:52 +0000

    Added run_tests.py. It's a simple wrapper around run_remote_tests.sh.
    PRESUBMIT=passed
    R=bjanakiraman,raymes
    DELTA=59  (59 added, 0 deleted, 0 changed)
    OCL=45051-p2
    RCL=45095-p2
    RDATE=2010/11/08 17:14:30
    
    
    P4 change: 42605663

---
+++ v14/run_tests.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Script to wrap run_remote_tests.sh script.
+
+This script calls run_remote_tests.sh with standard tests.
+"""
+
+__author__ = "asharif@google.com (Ahmad Sharif)"
+
+import optparse
+import os
+import sys
+from utils import utils
+
+# Common initializations
+(rootdir, basename) = utils.GetRoot(sys.argv[0])
+utils.InitLogger(rootdir, basename)
+
+
+def Main():
+  """The main function."""
+  parser = optparse.OptionParser()
+  parser.add_option("-c", "--chromeos_root", dest="chromeos_root",
+                    help="ChromeOS root checkout directory.")
+  parser.add_option("-r", "--remote", dest="remote",
+                    help="The IP address of the remote ChromeOS machine.")
+  parser.add_option("-b", "--board", dest="board",
+                    help="The board of the target.")
+
+  tests = "BuildVerify"
+
+  (options, args) = parser.parse_args()
+
+  if options.board is None or options.remote is None:
+    parser.print_help()
+    sys.exit()
+
+  if options.chromeos_root is None:
+    options.chromeos_root = "../.."
+
+  tests += " " + " ".join(args)
+  return RunRemoteTests(options.chromeos_root, options.remote,
+                        options.board, tests)
+
+
+def RunRemoteTests(chromeos_root, remote, board, tests):
+  """Run the remote tests."""
+  command = (chromeos_root + "/src/scripts/run_remote_tests.sh" +
+             " --remote=" + remote +
+             " --board=" + board +
+             " " + tests)
+
+  retval = utils.RunCommand(command)
+  return retval
+
+if __name__ == "__main__":
+  Main()
