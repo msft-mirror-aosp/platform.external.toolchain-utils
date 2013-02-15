cd9e629578b145bef699de4a7f64d1e1a0b48314
asharif <asharif@p4.invalid>
2013-02-15 09:05:01 +0000

    Added enable-checking=release for both Atom/ARM arches.  Added disable-esp configure option for ARM.
    PRESUBMIT=passed
    R=raymes,bjanakiraman
    DELTA=22  (3 added, 1 deleted, 18 changed)
    OCL=49297-p2
    RCL=49301-p2
    RDATE=2011/03/02 13:34:39
    
    
    P4 change: 42617271

--- v14/build-gcc/opts.sh
+++ v14/build-gcc/opts.sh
@@ -2,23 +2,26 @@ get_gcc_configure_options()
 {
   local CTARGET=$1; shift
   local confgcc=$(get_gcc_common_options)
-	case ${CTARGET} in
-		arm*)	#264534
-			local arm_arch="${CTARGET%%-*}"
-			# Only do this if arm_arch is armv*
-			if [[ ${arm_arch} == armv* ]] ; then
-				# Convert armv7{a,r,m} to armv7-{a,r,m}
-				[[ ${arm_arch} == armv7? ]] && arm_arch=${arm_arch/7/7-}
-				# Remove endian ('l' / 'eb')
-				[[ ${arm_arch} == *l  ]] && arm_arch=${arm_arch%l}
-				[[ ${arm_arch} == *eb ]] && arm_arch=${arm_arch%eb}
-				confgcc="${confgcc} --with-arch=${arm_arch}"
-			fi
-			;;
-		i?86*)
-			confgcc="${confgcc} --with-arch=atom"
-			;;
-	esac
+  case ${CTARGET} in
+    arm*)	#264534
+      local arm_arch="${CTARGET%%-*}"
+      # Only do this if arm_arch is armv*
+      if [[ ${arm_arch} == armv* ]] ; then
+        # Convert armv7{a,r,m} to armv7-{a,r,m}
+        [[ ${arm_arch} == armv7? ]] && arm_arch=${arm_arch/7/7-}
+        # Remove endian ('l' / 'eb')
+        [[ ${arm_arch} == *l  ]] && arm_arch=${arm_arch%l}
+        [[ ${arm_arch} == *eb ]] && arm_arch=${arm_arch%eb}
+        confgcc="${confgcc} --with-arch=${arm_arch}"
+        confgcc="${confgcc} --disable-esp"
+      fi
+      ;;
+    i?86*)
+      # Hardened is enabled for x86, but disabled for ARM.
+      confgcc="${confgcc} --with-arch=atom"
+      confgcc="${confgcc} --enable-esp"
+      ;;
+  esac
   echo ${confgcc}
 }
 
@@ -29,8 +32,7 @@ get_gcc_common_options()
   confgcc="${confgcc} --disable-libmudflap"
   confgcc="${confgcc} --disable-libssp"
   confgcc="${confgcc} --disable-libgomp"
-  # Hardened option.
-  confgcc="${confgcc} --enable-esp"
+  confgcc="${confgcc} --enable-checking=release"
   echo ${confgcc}
 }
 
