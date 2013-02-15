c89b8575c5897a60f8f793859d2811d8139602fa
raymes <raymes@p4.invalid>
2013-02-15 09:05:42 +0000

    Modified run_dejagnu.py: 1) Moved ssh logic out of remote_init and into run_dejagnu.py 2) Fixed bug which was causing ssh to fail (missing -i param) 3) Made it work with vanilla toolchains, as well as mounted ones 4) Made it work with all boards (instead of just x86)
    PRESUBMIT=passed
    R=asharif,bjanakiraman
    DELTA=98  (44 added, 39 deleted, 15 changed)
    OCL=49425-p2
    RCL=49429-p2
    RDATE=2011/03/04 15:20:07
    
    
    P4 change: 42617501

--- v14/run_dejagnu.py
+++ v14/run_dejagnu.py
@@ -11,10 +11,12 @@ import optparse
 import os
 import sys
 import build_chromeos
+from utils import command_executer
 from utils import utils
 from utils import logger
 
-DEJAGNU_DIR = "/usr/local/toolchain_root/v14/dejagnu"
+
+DEJAGNU_DIR = "/tmp/dejagnu"
 
 
 def Usage(parser, message):
@@ -37,14 +39,20 @@ def Main(argv):
                     help="The IP address of the machine to run the tests on")
   parser.add_option("--testflags", dest="testflags", default="",
                     help="Arguments to pass to DejaGNU.")
+  parser.add_option("--vanilla", dest="vanilla", default=False,
+                    action="store_true",
+                    help="Use the toolchain inside the chroot to run tests.")
 
   options = parser.parse_args(argv[1:])[0]
 
   if options.chromeos_root is None:
     Usage(parser, "--chromeos_root must be set")
 
-  if options.toolchain_root is None:
-    Usage(parser, "--toolchain_root must be set")
+  if options.toolchain_root is None and options.vanilla == False:
+    Usage(parser, "--toolchain_root or --vanilla must be set")
+
+  if options.toolchain_root is not None and options.vanilla == True:
+    Usage(parser, "If --vanilla specified, cannot use --toolchain_root")
 
   if options.remote is None:
     Usage(parser, "--remote must be set")
@@ -75,9 +83,17 @@ def Main(argv):
   target = f.read()
   f.close()
   target = target.strip()
-  gcc_build_dir = ("/usr/local/toolchain_root/output/objects"
-                   "/portage/cross-%s/gcc-9999/work/build"
-                   % target)
+  if options.vanilla:
+    gcc_build_dir = "/var/tmp/portage/cross-%s/gcc-*/work/build" % target
+  else:
+    gcc_build_dir = ("/usr/local/toolchain_root/output/objects"
+                     "/portage/cross-%s/gcc-9999/work/build"
+                     % target)
+
+  # Copy the dejagnu site.exp into the chroot.
+  ce = command_executer.GetCommandExecuter()
+  ce.CopyFiles(utils.GetRoot(sys.argv[0])[0] + "/dejagnu",
+               options.chromeos_root + "/chroot/" + DEJAGNU_DIR, recursive=True)
 
   # Construct the command to run DejaGNU.
   dejagnu_run = ("DEJAGNU=%s/site.exp DEJAGNU_HOSTNAME=%s make "
@@ -85,18 +101,19 @@ def Main(argv):
                  % (DEJAGNU_DIR, options.remote, options.testflags))
 
   # Construct command to init the ssh tcp connection 
-  init = "%s/remote_init.sh --init --remote=%s" % (DEJAGNU_DIR, options.remote)
+  init = GetRemoteAccessInitCommand(options.remote)
 
   # Construct the command to cleanup the ssh tcp connection
-  cleanup = ("%s/remote_init.sh --cleanup --remote=%s" %
-             (DEJAGNU_DIR, options.remote))
+  cleanup = GetRemoteAccessCleanupCommand()
+
+  common = GetRemoteAccessCommonCommands(options.remote)
 
   # Run DejaGNU
   ret = (build_chromeos.
          ExecuteCommandInChroot(options.chromeos_root, options.toolchain_root,
-                                "%s ; %s && cd %s && %s ; %s" %
-                                (cleanup, init, gcc_build_dir, dejagnu_run, cleanup),
-                                full_mount=True))
+                                "%s ; %s ; %s && cd %s && %s ; %s" %
+                                (common, cleanup, init, gcc_build_dir,
+                                 dejagnu_run, cleanup), full_mount=True))
   utils.AssertWarning(ret == 0, "Failed to run DejaGNU tests successfully")
 
   # Copy results to a not-so-deep location
@@ -104,7 +121,7 @@ def Main(argv):
   new_results_dir = "/usr/local/toolchain_root/output/dejagnu/"
   ret = (build_chromeos.
          ExecuteCommandInChroot(options.chromeos_root, options.toolchain_root,
-                                "mkdir -p %s ; cp %s/g++/g++.log %s ; "
+                                "sudo mkdir -p %s ; cp %s/g++/g++.log %s ; "
                                 "cp %s/gcc/gcc.log %s" %
                                 (new_results_dir, results_dir, new_results_dir,
                                  results_dir, new_results_dir)))
@@ -112,7 +129,34 @@ def Main(argv):
   utils.AssertWarning(ret == 0, "Failed to copy results to final destination.")
 
 
+def GetRemoteAccessCommonCommands(remote):
+    command = "\nset -- --remote=" + remote
+    command += "\n. ~/trunk/src/scripts/common.sh"
+    command += "\n. ~/trunk/src/scripts/remote_access.sh"
+    command += "\nTMP=/tmp/chromeos-toolchain"
+    command += "\nFLAGS \"$@\" || exit 1"
+    return command
 
+def GetRemoteAccessInitCommand(remote):
+  command = ("echo \"Initting access\""
+             " && mkdir -p ${TMP}"
+             " && remote_access_init"
+             " && ( ssh -t -t -p 22 root@%s"
+             " -o StrictHostKeyChecking=no"
+             " -o UserKnownHostsFile=/tmp/chromeos-toolchain/known_hosts"
+             " -i ${TMP}/private_key"
+             " -M -S ${TMP}/%%r@%%h:%%p 2>&1 > /dev/null & )"
+             " ; echo $! > ${TMP}/master-pid" % remote)
+  return command
+
+def GetRemoteAccessCleanupCommand():
+  command = ("echo \"Cleaning up access\""
+             " && set +e "
+             " && kill -9 `cat ${TMP}/master-pid`"
+             " && set -e"
+             " && cleanup_remote_access"
+             " && rm -rf ${TMP}")
+  return command
 
 
 if __name__ == "__main__":
