1d9986140f8a284ed34f873c89b02c94eb2a23a6
raymes <raymes@p4.invalid>
2013-02-15 21:14:50 +0000

    Added PRESUBMIT.py for automation to notify bhaskar-staff of automation-related changes.
    PRESUBMIT=passed
    R=asharif,kbaclawski,bjanakiraman
    APPROVED=kbaclawski
    DELTA=14  (14 added, 0 deleted, 0 changed)
    OCL=55119-p2
    RCL=55129-p2
    RDATE=2011/08/17 10:41:00
    
    
    P4 change: 42652692

---
+++ v14/automation/PRESUBMIT.py
@@ -0,0 +1,14 @@
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# Presubmit script for cc'ing bhaskar-staff on automation related CL's.
+#
+# PRESUBMIT METADATA:
+# [
+# MailTo(
+#   p4_filespecs = [
+#     "//depot2/gcctools/chromeos/v14/automation/...",
+#     ],
+#   addresses = ["bhaskar-staff"],
+#   owners = ["kbaclawski", "asharif", "raymes"]
+#   ),
+# ]
