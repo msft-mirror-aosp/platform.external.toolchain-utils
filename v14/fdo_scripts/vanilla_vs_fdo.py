63201e64cc3725b119a6c53242b3e8b7d22ac2d1
asharif <asharif@p4.invalid>
2013-02-16 02:41:07 +0000

    vanilla_vs_fdo.py now builds a Chrome with -fprofile-generate and uses it to build with -fprofile-use (in addition to USE="+pgo" vs USE="-pgo"). Also added an option to do the aforementioned steps on the chromeos-chrome-9999 ebuild.
    PRESUBMIT=passed
    R=bjanakiraman,cmtice,llozano
    DELTA=193  (167 added, 0 deleted, 26 changed)
    OCL=59063-p2
    RCL=59183-p2
    RDATE=2012/03/15 16:00:23
    USE="-pgo").
    
    
    P4 change: 42673868

--- v14/fdo_scripts/vanilla_vs_fdo.py
+++ v14/fdo_scripts/vanilla_vs_fdo.py
@@ -4,52 +4,130 @@
 
 """Script to profile a page cycler, and get it back to the host."""
 
+import getpass
 import optparse
 import os
 import sys
 
-import build_chrome_browser
+import image_chromeos
 import setup_chromeos
 from utils import command_executer
 from utils import misc
+from utils import logger
+
+
+class Patcher(object):
+  def __init__(self, dir_to_patch, patch_file):
+    self._dir_to_patch = dir_to_patch
+    self._patch_file = patch_file
+    self._base_patch_command = "patch -p0 %%s < %s" % patch_file
+    self._ce = command_executer.GetCommandExecuter()
+
+  def _RunPatchCommand(self, args):
+    patch_command = self._base_patch_command % args
+    command = ("cd %s && %s" % (self._dir_to_patch,
+                                patch_command))
+    return self._ce.RunCommand(command)
+
+  def _ApplyPatch(self, args):
+    full_args = "%s --dry-run" % args
+    ret = self._RunPatchCommand(full_args)
+    if ret:
+      raise Exception("Patch dry run failed!")
+    ret = self._RunPatchCommand(args)
+    if ret:
+      raise Exception("Patch application failed!")
+
+  def __enter__(self):
+    self._ApplyPatch("")
+
+  def __exit__(self, type, value, traceback):
+    self._ApplyPatch("-R")
 
 
 class FDOComparator(object):
-  def __init__(self, board, remotes):
+  def __init__(self, board, remotes, ebuild_version):
     self._board = board
     self._remotes = remotes
+    self._ebuild_version = ebuild_version
+    self._remote = remotes.split(",")[0]
     self._chromeos_root = "chromeos"
+    self._profile_dir = "profile_dir"
+    self._profile_path = os.path.join(self._chromeos_root,
+                                      "src",
+                                      "scripts",
+                                      os.path.basename(self._profile_dir))
+
     self._ce = command_executer.GetCommandExecuter()
+    self._l = logger.GetLogger()
 
   def _CheckoutChromeOS(self):
     if not os.path.exists(self._chromeos_root):
-      setup_chromeos_args = ["--dir=%s" % self._chromeos_root,
+      setup_chromeos_args = [setup_chromeos.__file__,
+                             "--dir=%s" % self._chromeos_root,
                              "--minilayout"]
       setup_chromeos.Main(setup_chromeos_args)
 
   def _BuildChromeOSUsingBinaries(self):
-    command = misc.GetSetupBoardCommand(self._board,
-                                         usepkg=True)
-    ret = self._ce.ChrootRunCommand(self._chromeos_root,
-                                    command)
+    image_dir = misc.GetImageDir(self._chromeos_root, self._board)
+    command = "equery-%s l chromeos" % self._board
+    ret = self._ce.ChrootRunCommand(self._chromeos_root, command)
     if ret:
-      raise Exception("Couldn't run setup_board!")
-    command = misc.GetBuildPackagesCommand(self._board,
-                                            True)
-    ret = self._ce.ChrootRunCommand(self._chromeos_root,
+      command = misc.GetSetupBoardCommand(self._board,
+                                           usepkg=True)
+      ret = self._ce.ChrootRunCommand(self._chromeos_root,
+                                      command)
+      if ret:
+        raise Exception("Couldn't run setup_board!")
+      command = misc.GetBuildPackagesCommand(self._board,
+                                              True)
+      ret = self._ce.ChrootRunCommand(self._chromeos_root,
                                     command)
-    if ret:
-      raise Exception("Couldn't run build_packages!")
+      if ret:
+        raise Exception("Couldn't run build_packages!")
 
-  def _BuildChromeAndImage(self, env_dict):
+  def _ReportMismatches(self, build_log):
+    mismatch_signature = "-Wcoverage-mismatch"
+    mismatches = build_log.count(mismatch_signature)
+    self._l.LogOutput("Total mismatches: %s" % mismatches)
+    stale_files = set([])
+    for line in build_log.splitlines():
+      if mismatch_signature in line:
+        filename = line.split(":")[0]
+        stale_files.add(filename)
+    self._l.LogOutput("Total stale files: %s" % len(stale_files))
+
+  def _BuildChromeAndImage(self, ebuild_version="", env_dict={}, cflags="",
+                           cxxflags="", ldflags="", label=""):
     env_string = misc.GetEnvStringFromDict(env_dict)
-    label = misc.GetFilenameFromString(env_string)
+    if not label:
+      label = " ".join([env_string,
+                        cflags,
+                        cxxflags,
+                        ldflags,
+                        ebuild_version])
+      label = label.strip()
+      label = misc.GetFilenameFromString(label)
     if not misc.DoesLabelExist(self._chromeos_root, self._board, label):
-      build_chrome_browser_args = ["--chromeos_root=%s" % self._chromeos_root,
+      build_chrome_browser_args = ["--clean",
+                                   "--chromeos_root=%s" % self._chromeos_root,
                                    "--board=%s" % self._board,
-                                   "--env=%s" %
-                                   env_string]
-      ret = build_chrome_browser.Main(build_chrome_browser_args)
+                                   "--env=%r" % env_string,
+                                   "--cflags=%r" % cflags,
+                                   "--cxxflags=%r" % cxxflags,
+                                   "--ldflags=%r" % ldflags,
+                                   "--ebuild_version=%s" % ebuild_version]
+
+      build_chrome_browser = os.path.join(os.path.dirname(__file__),
+                                          "..",
+                                          "build_chrome_browser.py")
+      command = "python %s %s" % (build_chrome_browser,
+                                  " ".join(build_chrome_browser_args))
+      ret, out, err = self._ce.RunCommand(command,
+                                          return_output=True)
+      if "-fprofile-use" in cxxflags:
+        self._ReportMismatches(out)
+
       if ret:
         raise Exception("Couldn't build chrome browser!")
       misc.LabelLatestImage(self._chromeos_root, self._board, label)
@@ -81,7 +159,7 @@ class FDOComparator(object):
         """ % (crosperf_label,
                os.path.join(misc.GetImageDir(self._chromeos_root, self._board),
                             label,
-                            "chromiumos_image.bin"))
+                            "chromiumos_test_image.bin"))
         print >>f, experiment_image
     crosperf = os.path.join(os.path.dirname(__file__),
                             "..",
@@ -92,12 +170,97 @@ class FDOComparator(object):
     if ret:
       raise Exception("Couldn't run crosperf!")
 
+  def _ImageRemote(self, label):
+    image_path = os.path.join(misc.GetImageDir(self._chromeos_root,
+                                                self._board),
+                              label,
+                              "chromiumos_test_image.bin")
+    image_chromeos_args = [image_chromeos.__file__,
+                           "--chromeos_root=%s" % self._chromeos_root,
+                           "--image=%s" % image_path,
+                           "--remote=%s" % self._remote,
+                           "--board=%s" % self._board]
+    image_chromeos.Main(image_chromeos_args)
+
+  def _ProfileRemote(self):
+    profile_cycler = os.path.join(os.path.dirname(__file__),
+                                  "profile_cycler.py")
+    profile_cycler_args = ["--chromeos_root=%s" % self._chromeos_root,
+                           "--cycler=all",
+                           "--board=%s" % self._board,
+                           "--profile_dir=%s" % self._profile_path,
+                           "--remote=%s" % self._remote]
+    command = "python %s %s" % (profile_cycler, " ".join(profile_cycler_args))
+    ret = self._ce.RunCommand(command)
+    if ret:
+      raise Exception("Couldn't profile cycler!")
+
+  def _BuildGenerateImage(self):
+    # TODO(asharif): add cflags as well.
+    # TODO(asharif): Should not have to apply this patch...
+    # First patch the tree
+    with Patcher(os.path.join(self._chromeos_root,
+                              "chroot",
+                              misc.GetChromeSrcDir()),
+                 "/home/asharif/fdo_scripts/pgo.patch"):
+      cxxflags = "-fprofile-generate -w"
+      labels_list = ["fprofile-generate", self._ebuild_version]
+      label = "_".join(labels_list)
+      generate_label = self._BuildChromeAndImage(
+          env_dict={"USE": "-pgo"},
+          cxxflags=cxxflags,
+          ldflags="%s -lgcov" % cxxflags,
+          label=label,
+          ebuild_version=self._ebuild_version)
+      return generate_label
+
+  def _BuildUseImage(self):
+    ctarget = misc.GetCtargetFromBoard(self._board, self._chromeos_root)
+    chroot_profile_dir = os.path.join("/home/%s/trunk" % getpass.getuser(),
+                                      "src",
+                                      "scripts",
+                                      self._profile_dir,
+                                      ctarget)
+    cflags = ("-fprofile-use "
+              "-fprofile-correction "
+              "-Wno-error "
+              "-fopt-info=0 "
+              "-fno-vpt "
+              "-fprofile-dir=%s" %
+              chroot_profile_dir)
+    labels_list = ["updated_pgo", self._ebuild_version]
+    label = "_".join(labels_list)
+    pgo_use_label = self._BuildChromeAndImage(
+        env_dict={"USE": "-pgo"},
+        cflags=cflags,
+        cxxflags=cflags,
+        ldflags=cflags,
+        label=label,
+        ebuild_version=self._ebuild_version)
+    return pgo_use_label
+
   def DoAll(self):
     self._CheckoutChromeOS()
     self._BuildChromeOSUsingBinaries()
-    vanilla_label = self._BuildChromeAndImage({"USE": "-pgo"})
-    pgo_label = self._BuildChromeAndImage({"USE": "+pgo"})
-    self._TestLabels([vanilla_label, pgo_label])
+
+    vanilla_label = self._BuildChromeAndImage(env_dict={"USE": "-pgo"},
+                                              ebuild_version=self._ebuild_version)
+    pgo_label = self._BuildChromeAndImage(env_dict={"USE": "+pgo"},
+                                          ebuild_version=self._ebuild_version)
+    #self._TestLabels([vanilla_label, pgo_label])
+
+    if not os.path.exists(self._profile_path):
+      # Build Chrome with -fprofile-generate
+      generate_label = self._BuildGenerateImage()
+      # Image to the remote box.
+      self._ImageRemote(generate_label)
+      # Profile it using all page cyclers.
+      self._ProfileRemote()
+
+    # Use the profile directory to rebuild it.
+    updated_pgo_label = self._BuildUseImage()
+    # Run crosperf on all images now.
+    self._TestLabels([vanilla_label, pgo_label, updated_pgo_label])
     return 0
 
 
@@ -114,6 +277,10 @@ def Main(argv):
                     dest="board",
                     default="x86-zgb",
                     help="The target board.")
+  parser.add_option("--ebuild_version",
+                    dest="ebuild_version",
+                    default="",
+                    help="The Chrome ebuild version to use.")
   options, _ = parser.parse_args(argv)
   if not options.board:
     print "Please give a board."
@@ -121,7 +288,7 @@ def Main(argv):
   if not options.remote:
     print "Please give at least one remote machine."
     return 1
-  fc = FDOComparator(options.board, options.remote)
+  fc = FDOComparator(options.board, options.remote, options.ebuild_version)
   return fc.DoAll()
 
 
