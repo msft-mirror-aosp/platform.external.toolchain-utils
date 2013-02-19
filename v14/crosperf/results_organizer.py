bb8f6791bf2012d8112a584f9fd5fb0777ada045
yunlian <yunlian@p4.invalid>
2013-02-19 19:56:26 +0000

    Change the output format of result_organizer to make it suite the tabulator. Test = looked the output of result_organizer from the crosperf,        and it looks fine.
    PRESUBMIT=passed
    R=asharif,shenhan
    APPROVED=asharif
    DELTA=28  (2 added, 15 deleted, 11 changed)
    OCL=62741-p2
    RCL=62771-p2
    RDATE=2012/09/14 16:03:04
    
    
    P4 change: 42793108

--- v14/crosperf/results_organizer.py
+++ v14/crosperf/results_organizer.py
@@ -7,14 +7,11 @@ class ResultOrganizer(object):
   """Create a dict from benchmark_runs.
 
   The structure of the output dict is as follows:
-  {"benchmark_1":
-    [
-     {"autotest_key1":[v1, v2, v3,,,],
-      "autotest_key2":[v1, v2, v3,,,]}
-     #label_1
-
-     {...}
-     #lable_2
+  {"benchmark_1":[
+    [{"key1":"v1", "key2":"v2"},{"key1":"v1", "key2","v2"}]
+    #one label
+    []
+    #the other label
     ]
    "benchmark_2":
     [
@@ -31,25 +28,15 @@ class ResultOrganizer(object):
       if benchmark_name not in self.result:
         self.result[benchmark_name] = []
         while len(self.result[benchmark_name]) < len(labels):
-          self.result[benchmark_name].append({})
-      cur_table = self.result[benchmark_name]
-      label_name = benchmark_run.label_name
-      label_index = self.labels.index(label_name)
-      cur_dict = cur_table[label_index]
+          self.result[benchmark_name].append([])
+      label_index = self.labels.index(benchmark_run.label_name)
+      cur_table = self.result[benchmark_name][label_index]
+      index = benchmark_run.iteration - 1
+      while index >= len(cur_table):
+        cur_table.append({})
+      cur_dict = cur_table[index]
       if not benchmark_run.result:
         continue
       for autotest_key in benchmark_run.result.keyvals:
-        if autotest_key not in cur_dict:
-          cur_dict[autotest_key] = []
-        cell = cur_dict[autotest_key]
-        index = benchmark_run.iteration - 1
-        while index >= len(cell):
-          cell.append(None)
-
         result_value = benchmark_run.result.keyvals[autotest_key]
-        try:
-          result_value = float(result_value)
-        except ValueError:
-          pass
-
-        cell[index] = result_value
+        cur_dict[autotest_key] = result_value
