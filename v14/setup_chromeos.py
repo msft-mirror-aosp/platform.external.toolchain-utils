992a4903147eb1243113d6d6140b7d3b77972338
yunlian <yunlian@p4.invalid>
2013-02-19 20:19:25 +0000

    Correct minilayout parameter
    Test: run setup_chromeos.py --dir=blabla --minilayout passed
    
    PRESUBMIT=passed
    R=asharif
    APPROVED=asharif
    DELTA=1  (0 added, 0 deleted, 1 changed)
    RCL=63085-p2
    RDATE=2012/09/28 15:38:10
    
    
    P4 change: 42794995

--- v14/setup_chromeos.py
+++ v14/setup_chromeos.py
@@ -126,7 +126,7 @@ the version option.""")
     init = ("repo init -u %s -m paladin/buildspecs/%s/%s.xml" % (
             versions_repo, version, manifest))
   if options.minilayout:
-    init += " -g minilayout.xml"
+    init += " -g minilayout"
 
   init += " --repo-url=http://git.chromium.org/external/repo.git"
 
