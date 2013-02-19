c742d992e0dc413903e44aa5800c1449da026a45
yunlian <yunlian@p4.invalid>
2013-02-19 20:32:01 +0000

    Add timeline class to support debug Test: unittest python timeline_test.py passed
    PRESUBMIT=passed
    R=asharif
    APPROVED=asharif
    DELTA=86  (86 added, 0 deleted, 0 changed)
    OCL=63344-p2
    RCL=63346-p2
    RDATE=2012/10/12 12:37:35
    
    
    P4 change: 42796039

---
+++ v14/utils/timeline.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2.6
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+
+"""Tools for recording and reporting timeline of benchmark_run."""
+
+__author__ = 'yunlian@google.com (Yunlian Jiang)'
+
+import time
+
+
+class Event(object):
+  def __init__(self, name='', cur_time=0):
+    self.name = name
+    self.timestamp = cur_time
+
+
+class Timeline(object):
+  """Use a dict to store the timeline."""
+
+  def __init__(self):
+    self.events = []
+
+  def Record(self, event):
+    for e in self.events:
+      assert e.name != event, ('The event {0} is already recorded.'
+                               .format(event))
+    cur_event = Event(name=event, cur_time=time.time())
+    self.events.append(cur_event)
+
+  def GetEvents(self):
+    return([e.name for e in self.events])
+
+  def GetEventDict(self):
+    tl = {}
+    for e in self.events:
+      tl[e.name] = e.timestamp
+    return tl
+
+  def GetEventTime(self, event):
+    for e in self.events:
+      if e.name == event:
+        return e.timestamp
+    raise IndexError, 'The event {0} is not recorded'.format(event)
