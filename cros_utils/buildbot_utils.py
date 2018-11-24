5cc59a85d1557f9fb420f6a718c201e6c62c2a53
Caroline Tice <cmtice@google.com>
2018-11-23 16:52:17 -0800

    [toolchain-utils] Increase timeout for buildbot tryjobs.
    Recent infrastructure changes have added some stages to our trybot
    jobs making them take longer, and causing them to hit our timeout
    limit.  This CL increases the timeout limit.
    
    BUG=chromium:908188
    TEST=None
    
    Change-Id: I19bab9d5ee8d0d4074575a1a719fa23fe5de6704
    Reviewed-on: https://chromium-review.googlesource.com/1349479
    Commit-Ready: Caroline Tice <cmtice@chromium.org>
    Tested-by: Caroline Tice <cmtice@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>

--- cros_utils/buildbot_utils.py
+++ cros_utils/buildbot_utils.py
@@ -17,9 +17,9 @@ from cros_utils import logger
 INITIAL_SLEEP_TIME = 7200  # 2 hours; wait time before polling buildbot.
 SLEEP_TIME = 600  # 10 minutes; time between polling of buildbot.
 
-# Some of our slower builders (llmv-next) are taking more
-# than 8 hours. So, increase this TIME_OUT to 9 hours.
-TIME_OUT = 32400  # Decide the build is dead or will never finish
+# Some of our slower builders (llvm-next) are taking more
+# than 8 hours. So, increase this TIME_OUT to 10.5 hours.
+TIME_OUT = 37800  # Decide the build is dead or will never finish
 
 
 class BuildbotTimeout(Exception):
