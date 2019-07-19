f650e84d94539a3704f3b47d8c3c320444b14189
Emma Vukelj <emmavukelj@google.com>
2019-07-19 14:38:21 -0700

    Clang-Tidy: Remove clang tidy project from tc-utils
    This CL removes all files related to the Clang Tidy Warnings for Chrome
    in Chrome OS project from toolchain-utils, as these files have been
    migrated to google3 and will now be run by a borg job rather than by the
    Chrome OS builder.
    
    BUG=None
    TEST=None
    
    Change-Id: If656b967f7f56998754667e0eac1eba11e3b619f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/1711060
    Reviewed-by: Caroline Tice <cmtice@chromium.org>
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Tested-by: Emma Vukelj <emmavukelj@google.com>

--- clang_tidy/compile_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-# This script generates warnings_pb2.py for use by clang_tidy_warn.py and
-# clang_tidy_warn_test.py.
-#
-# This script is dependent on having protoc. If you do not have it, check
-# Team Tools and Scripts for installing protoc
-mydir=$(dirname "$(readlink -m "$0")")
-protoc --python_out="${mydir}" --proto_path "${mydir}" warnings.proto
