265c29673103ec2b03a3cd81d065537a53122187
Denis Nikitin <denik@google.com>
2019-08-01 19:52:49 -0700

    toolchain-utils: Add no_hwp to nightly perf test
    Added --no_hwp argument to crosperf to run benchmark tests with
    intel_pstate=no_hwp kernel command line option.
    The option disables Intel's HW control of CPU frequency.
    CPU freq drops to fixed base frequency.
    
    BUG=chromium:966514
    TEST=tested on samus, eve, kefka, rammus, scarlet, nami.
    
    Cq-Depend: chromium:1726635
    Change-Id: I6ec2b5b04a4101d4c8ef7c0fbc5acc43f439275f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/1732431
    Reviewed-by: Denis Nikitin <denik@chromium.org>
    Tested-by: Denis Nikitin <denik@chromium.org>

--- buildbot_test_toolchains.py
+++ buildbot_test_toolchains.py
@@ -4,6 +4,7 @@
 # Copyright 2016 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 """Script for running nightly compiler tests on ChromeOS.
 
 This script launches a buildbot to build ChromeOS with the latest compiler on
@@ -214,7 +215,7 @@ class ToolchainComparator(object):
 
     crosperf = os.path.join(TOOLCHAIN_DIR, 'crosperf', 'crosperf')
     noschedv2_opts = '--noschedv2' if self._noschedv2 else ''
-    command = ('{crosperf} --no_email=True --results_dir={r_dir} '
+    command = ('{crosperf} --no_email=True --results_dir={r_dir} --no_hwp '
                '--json_report=True {noschedv2_opts} {exp_file}').format(
                    crosperf=crosperf,
                    r_dir=self._reports_dir,
