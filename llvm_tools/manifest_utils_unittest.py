ce681148809afa88dd5a27c111fc6d1410058f1c
Jordan R Abrahams-Whitehead <ajordanr@google.com>
2023-05-24 23:02:24 +0000

    llvm_tools: Add manifest_utils
    This is part of the effort to get LLVM 9999 ebuilds working.
    Because LLVM 9999 ebuilds need to know what revision LLVM is at,
    and because that info is only available during unpack time currently,
    we need to move the LLVM_HASH into the manifest files.
    
    Having the LLVM_HASH in the manifest allows repo init to pull down
    the correct LLVM revision in the source tree checkout. This then
    allows chromeos-version.sh to determine what the new ebuild name
    should be during the PUpr rename stage. We could either parse
    the ebuild via chromeos-version.sh, or we could update the manifest.
    
    This is part of the effort for the latter implementation.
    
    This library contains functions that let us locate and update
    the manifest file, which we want to be as seamless as possible
    for toolchain maintainers in ChromeOS.
    
    BUG=b:275877115
    TEST=manifest_utils_unittest.py
    
    Change-Id: I45b8ebc72d5e6462552e6873d897199481007f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/4575384
    Tested-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Reviewed-by: Ryan Beltran <ryanbeltran@chromium.org>
    Commit-Queue: Jordan Abrahams-Whitehead <ajordanr@google.com>

---
+++ llvm_tools/manifest_utils_unittest.py
@@ -0,0 +1,75 @@
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import io
+from pathlib import Path
+import re
+import unittest
+from xml.etree import ElementTree
+
+import manifest_utils
+
+
+"""Provides utilities to read and edit the ChromiumOS Manifest entries.
+
+While this code reads and edits the internal manifest, it should only operate
+on toolchain projects (llvm-project, etc.) which are public.
+"""
+
+MANIFEST_FIXTURE = """<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <include name="_remotes.xml" />
+  <default revision="refs/heads/main"
+           remote="cros"
+           sync-j="8" />
+
+  <include name="_kernel_upstream.xml" />
+
+  <!-- Common projects for developing CrOS. -->
+  <project path="src/repohooks"
+           name="chromiumos/repohooks"
+           groups="minilayout,paygen,firmware,buildtools,labtools,crosvm" />
+  <repo-hooks in-project="chromiumos/repohooks"
+              enabled-list="pre-upload" />
+  <project path="chromite"
+           name="chromiumos/chromite"
+           groups="minilayout,paygen,firmware,buildtools,chromeos-admin">
+    <copyfile src="AUTHORS" dest="AUTHORS" />
+    <copyfile src="LICENSE" dest="LICENSE" />
+  </project>
+  <project path="src/third_party/llvm-project"
+           name="external/github.com/llvm/llvm-project"
+           groups="notdefault,bazel"
+           revision="abcd" />
+  <project path="chromite/third_party/pyelftools"
+           name="chromiumos/third_party/pyelftools"
+           revision="refs/heads/chromeos-0.22"
+           groups="minilayout,paygen,firmware,buildtools" />
+</manifest>
+"""
+
+
+class TestManifestUtils(unittest.TestCase):
+    """Test manifest_utils."""
+
+    def test_update_chromeos_manifest(self):
+        root = ElementTree.fromstring(MANIFEST_FIXTURE)
+        manifest_utils.update_chromeos_manifest_tree("wxyz", root)
+        string_root1 = ElementTree.tostring(root)
+        self.assertRegex(
+            str(string_root1, encoding="utf-8"),
+            r'revision="wxyz"',
+        )
+        self.assertNotRegex(
+            str(string_root1, encoding="utf-8"),
+            r'revision="abcd"',
+        )
+        # Check idempotence.
+        manifest_utils.update_chromeos_manifest_tree("wxyz", root)
+        string_root2 = ElementTree.tostring(root)
+        self.assertEqual(string_root1, string_root2)
+
+
+if __name__ == "__main__":
+    unittest.main()
