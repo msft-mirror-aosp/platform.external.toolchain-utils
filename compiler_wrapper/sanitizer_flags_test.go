a053718c4c3146d9d27be389d7a44ef443389216
George Burgess IV <gbiv@google.com>
2023-07-25 01:22:59 -0600

    compiler_wrapper: disable _FORTIFY_SOURCE=3 for sanitizers
    Thanks to manojgupta for flagging this!
    
    BUG=b:288411201
    TEST=CQ+1, unittests
    
    Change-Id: Ice3e555cb20f93f971d4ec923b442f46786a7e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/4713974
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- compiler_wrapper/sanitizer_flags_test.go
+++ compiler_wrapper/sanitizer_flags_test.go
@@ -73,6 +73,42 @@ func TestFilterUnsupportedSanitizerFlagsIfSanitizeGiven(t *testing.T) {
 	})
 }
 
+func TestAllFortifyEnableFlagsAreRemoved(t *testing.T) {
+	withTestContext(t, func(ctx *testContext) {
+		flagsToRemove := []string{
+			"-D_FORTIFY_SOURCE=1",
+			"-D_FORTIFY_SOURCE=2",
+			"-D_FORTIFY_SOURCE=3",
+		}
+		for _, fortifyFlag := range flagsToRemove {
+			ctx.cfg.commonFlags = []string{fortifyFlag}
+			cmd := ctx.must(callCompiler(ctx, ctx.cfg,
+				ctx.newCommand(clangX86_64, "-fsanitize=kernel-address", mainCc)))
+			if err := verifyArgCount(cmd, 0, fortifyFlag); err != nil {
+				t.Errorf("Verifying FORTIFY flag %q is removed: %v", fortifyFlag, err)
+			}
+		}
+	})
+}
+
+func TestFortifyDisableFlagsAreKept(t *testing.T) {
+	withTestContext(t, func(ctx *testContext) {
+		flagsToKeep := []string{
+			"-D_FORTIFY_SOURCE",
+			"-D_FORTIFY_SOURCE=",
+			"-D_FORTIFY_SOURCE=0",
+		}
+		for _, fortifyFlag := range flagsToKeep {
+			ctx.cfg.commonFlags = []string{fortifyFlag}
+			cmd := ctx.must(callCompiler(ctx, ctx.cfg,
+				ctx.newCommand(clangX86_64, "-fsanitize=kernel-address", mainCc)))
+			if err := verifyArgCount(cmd, 1, fortifyFlag); err != nil {
+				t.Errorf("Verifying FORTIFY flag %q is kept: %v", fortifyFlag, err)
+			}
+		}
+	})
+}
+
 func TestFilterUnsupportedDefaultSanitizerFlagsIfSanitizeGivenForClang(t *testing.T) {
 	withTestContext(t, func(ctx *testContext) {
 		ctx.cfg.commonFlags = []string{"-D_FORTIFY_SOURCE=1"}
