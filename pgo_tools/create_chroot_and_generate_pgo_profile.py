90d828b872b2431d2b304723ebd1ace9ba84414f
George Burgess IV <gbiv@google.com>
2024-04-23 14:07:04 -0600

    pgo_tools: update PGO generation to detect bootstrap chroot version
    The `--bootstrap` flag is gone. Users must now detect and pass the
    bootstrap SDK version manually.
    
    BUG=b:335515781
    TEST=Ran the script
    
    Change-Id: I871611e70484c21390fc050955b8c8d792b20367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/5479388
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Bob Haarman <inglorion@chromium.org>

--- pgo_tools/create_chroot_and_generate_pgo_profile.py
+++ pgo_tools/create_chroot_and_generate_pgo_profile.py
@@ -13,6 +13,7 @@ import dataclasses
 import logging
 import os
 from pathlib import Path
+import re
 import shlex
 import shutil
 import subprocess
@@ -22,12 +23,24 @@ from typing import List
 import pgo_tools
 
 
+SDK_VERSION_CONF_SUBDIR = (
+    Path("src")
+    / "third_party"
+    / "chromiumos-overlay"
+    / "chromeos"
+    / "binhost"
+    / "host"
+    / "sdk_version.conf"
+)
+
+
 @dataclasses.dataclass(frozen=True)
 class ChrootInfo:
-    """Info that describes a unique chroot."""
+    """Describes a unique chroot, and the SDK version to pin it to."""
 
     chroot_name: str
     out_dir_name: str
+    sdk_version: str
 
 
 def find_repo_root(base_dir: Path) -> Path:
@@ -42,16 +55,37 @@ def find_repo_root(base_dir: Path) -> Path:
     raise ValueError(f"No repo found above {base_dir}")
 
 
-def create_fresh_bootstrap_chroot(repo_root: Path, chroot_info: ChrootInfo):
-    """Creates a `--bootstrap` chroot without any updates applied."""
+def detect_bootstrap_sdk_version(repo_root: Path) -> str:
+    sdk_version_conf = repo_root / SDK_VERSION_CONF_SUBDIR
+    bootstrap_version_re = re.compile(
+        r'^BOOTSTRAP_FROZEN_VERSION="([^"]+)"$',
+        re.MULTILINE,
+    )
+    results = bootstrap_version_re.findall(
+        sdk_version_conf.read_text(encoding="utf-8")
+    )
+    if len(results) != 1:
+        raise ValueError(
+            f"Expected exactly one match in {sdk_version_conf} for "
+            f"{bootstrap_version_re}; found {len(results)}"
+        )
+    return results[0]
+
+
+def create_fresh_chroot(
+    repo_root: Path,
+    chroot_info: ChrootInfo,
+):
+    """Creates a chroot. If it already exists, replaces it."""
     pgo_tools.run(
         [
             "cros_sdk",
             "--replace",
             f"--chroot={chroot_info.chroot_name}",
             f"--out-dir={chroot_info.out_dir_name}",
-            "--bootstrap",
-            "--skip-chroot-upgrade",
+            f"--sdk-version={chroot_info.sdk_version}",
+            "--",
+            "true",
         ],
         cwd=repo_root,
     )
@@ -68,7 +102,7 @@ def generate_pgo_profile(
             "cros_sdk",
             f"--chroot={chroot_info.chroot_name}",
             f"--out-dir={chroot_info.out_dir_name}",
-            "--skip-chroot-upgrade",
+            f"--sdk-version={chroot_info.sdk_version}",
             "--",
             "/mnt/host/source/src/third_party/toolchain-utils/pgo_tools/"
             "generate_pgo_profile.py",
@@ -177,15 +211,19 @@ def main(argv: List[str]):
     logging.info("Repo root is %s", repo_root)
 
     logging.info("Creating new SDK")
-    chroot_info = ChrootInfo(opts.chroot, opts.out_dir)
+    bootstrap_sdk_version = detect_bootstrap_sdk_version(repo_root)
+    logging.info("Detected bootstrap SDK version: %s", bootstrap_sdk_version)
+    bootstrap_chroot_info = ChrootInfo(
+        opts.chroot, opts.out_dir, bootstrap_sdk_version
+    )
     try:
-        create_fresh_bootstrap_chroot(repo_root, chroot_info)
+        create_fresh_chroot(repo_root, bootstrap_chroot_info)
         chroot_profile_path = Path("/tmp/llvm-next-pgo-profile.prof")
         generate_pgo_profile(
-            repo_root, chroot_info, chroot_profile_path
+            repo_root, bootstrap_chroot_info, chroot_profile_path
         )
         profile_path = translate_chroot_path_to_out_of_chroot(
-            repo_root, chroot_profile_path, chroot_info
+            repo_root, chroot_profile_path, bootstrap_chroot_info
         )
         if opts.output:
             shutil.copyfile(profile_path, opts.output)
@@ -207,16 +245,16 @@ def main(argv: List[str]):
         logging.warning(
             "NOTE: Chroot left at %s and out dir is left at %s. "
             "If you don't plan to rerun this script, delete them.",
-            chroot_info.chroot_name,
-            chroot_info.out_dir_name,
+            bootstrap_chroot_info.chroot_name,
+            bootstrap_chroot_info.out_dir_name,
         )
         raise
     else:
         logging.info(
             "Feel free to delete chroot %s and out dir %s when you're done "
             "with them.",
-            chroot_info.chroot_name,
-            chroot_info.out_dir_name,
+            bootstrap_chroot_info.chroot_name,
+            bootstrap_chroot_info.out_dir_name,
         )
 
 
--- pgo_tools/create_chroot_and_generate_pgo_profile_test.py
+++ pgo_tools/create_chroot_and_generate_pgo_profile_test.py
@@ -15,6 +15,27 @@ import unittest
 import create_chroot_and_generate_pgo_profile as create_chroot_etc
 
 
+EXAMPLE_SDK_VERSION_CONF_FILE = r"""
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# The last version of the sdk that we built & tested.
+SDK_LATEST_VERSION="2024.04.22.140014"
+
+# How to find the standalone toolchains from the above sdk.
+TC_PATH="2024/04/%(target)s-2024.04.22.140014.tar.xz"
+
+# Frozen version of SDK used for bootstrapping.
+# If unset, SDK_LATEST_VERSION will be used for bootstrapping.
+BOOTSTRAP_FROZEN_VERSION="2024.03.12.020106"
+
+# The Google Storage bucket containing the SDK tarball and toolchains.
+# If empty, Chromite will assume a default value, likely "chromiumos-sdk".
+SDK_BUCKET=""
+"""
+
+
 class Test(unittest.TestCase):
     """Tests for generate_llvm_next_pgo."""
 
@@ -23,11 +44,24 @@ class Test(unittest.TestCase):
         self.addCleanup(lambda: shutil.rmtree(tempdir))
         return tempdir
 
+    def test_sdk_version_detection_works(self):
+        repo_root = self.make_tempdir()
+        sdk_version_conf = repo_root / create_chroot_etc.SDK_VERSION_CONF_SUBDIR
+        sdk_version_conf.parent.mkdir(parents=True)
+        sdk_version_conf.write_text(
+            EXAMPLE_SDK_VERSION_CONF_FILE, encoding="utf-8"
+        )
+        self.assertEqual(
+            create_chroot_etc.detect_bootstrap_sdk_version(repo_root),
+            "2024.03.12.020106",
+        )
+
     def test_path_translation_works(self):
         repo_root = Path("/some/repo")
         chroot_info = create_chroot_etc.ChrootInfo(
             chroot_name="my-chroot",
             out_dir_name="my-out",
+            sdk_version="123",
         )
         self.assertEqual(
             create_chroot_etc.translate_chroot_path_to_out_of_chroot(
