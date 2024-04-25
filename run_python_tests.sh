64ca2b87b562b306263699a377b08875ddf176d5
George Burgess IV <gbiv@google.com>
2024-04-24 18:05:15 -0600

    add `run_python_tests.sh`
    This acts a convenient way for devs to run one or more Python tests.
    It's a lite wrapper around pytest, which is available in the chroot.
    
    BUG=b:336823685
    TEST=Ran the script
    
    Change-Id: I52452a9e1e1b1ba16c2e58e56b221bd71aed532b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/5486677
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

---
+++ run_python_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+my_dir="$(dirname "$(readlink -m "$0")")"
+cd "${my_dir}"
+
+if [[ ! -e /etc/cros_chroot_version ]]; then
+  echo "Re-exec'ing within the chroot..." >&2
+  exec cros_sdk \
+    --working-dir=. \
+    -- \
+    './run_python_tests.sh' \
+    "$@"
+fi
+
+# Note that PYTHONPATH is not necessary here for regular execution, but a
+# single script to run is specified, `pytest` will invoke it in a way that
+# breaks toolchain-utils module importing.
+#
+# Exemptions:
+# - git_llvm_rev_test is ignored because it takes a while. It'd be nice to
+#   optionally enable it, but it's realistically very unlikely to break.
+# - debug_info_test/debug_info_test.py is ignored, since that's the name of
+#   a non-test script, and pytest is confused by this.
+PYTHONPATH="${PWD}:${PYTHONPATH:-}" pytest \
+  --ignore=debug_info_test/debug_info_test.py \
+  --ignore=llvm_tools/llvm-project-copy \
+  --ignore=llvm_tools/git_llvm_rev_test.py \
+  "$@"
