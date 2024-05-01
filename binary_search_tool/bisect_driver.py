ae0dbb5e69b2cfda2341813fb1fe619cecba65fa
George Burgess IV <gbiv@google.com>
2024-05-01 15:00:57 -0600

    binary_search_tool: re-remove
    Now that this has been removed from ebuilds, it should be fine to
    delete.
    
    BUG=b:337857277
    TEST=Staging SDK builder invocation
    
    Change-Id: I789a48df7089dbb21a4feebbcdcc97c86dc08810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/5507513
    Tested-by: George Burgess <gbiv@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: Ryan Beltran <ryanbeltran@chromium.org>

--- binary_search_tool/bisect_driver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright 2024 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-import sys
-
-
-sys.exit("This script is deprecated. Please do not use it.")
