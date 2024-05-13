d072003ab7c74e5922920594500eec273f10a70a
George Burgess IV <gbiv@google.com>
2024-05-13 10:19:43 -0600

    llvm_tools: DCE `chroot`; add tests for root finding
    `rg` says the removed functions are no longer used.
    
    BUG=None
    TEST=Unittests
    
    Change-Id: I057b4ba002ac0bcfa0d0eb121669b7403c040190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/5533679
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Tested-by: George Burgess <gbiv@chromium.org>

--- llvm_tools/chroot.py
+++ llvm_tools/chroot.py
@@ -7,8 +7,6 @@
 
 import os
 from pathlib import Path
-import subprocess
-from typing import Iterable, List, Union
 
 
 def InChroot() -> bool:
@@ -22,7 +20,6 @@ def VerifyInsideChroot() -> None:
     Raises:
         AssertionError: The script was run outside the chroot.
     """
-
     assert InChroot(), "Script should be run inside the chroot."
 
 
@@ -32,21 +29,7 @@ def VerifyOutsideChroot() -> None:
     Raises:
         AssertionError: The script was run inside the chroot.
     """
-
     assert not InChroot(), "Script should be run outside the chroot."
-
-
-def VerifyChromeOSRoot(chromeos_root: Union[Path, str]) -> None:
-    """Checks whether the path actually points to ChromiumOS checkout root.
-
-    Raises:
-        AssertionError: The path is not ChromiumOS checkout root.
-    """
-
-    subdir = "src/third_party/chromiumos-overlay"
-    path = Path(chromeos_root).expanduser() / subdir
-    msg = f"Wrong ChromeOS path. No {subdir} directory in {chromeos_root} ."
-    assert path.is_dir(), msg
 
 
 def FindChromeOSRootAbove(chromeos_tree_path: Path) -> Path:
@@ -64,80 +47,3 @@ def FindChromeOSRootAbove(chromeos_tree_path: Path) -> Path:
         if (parent / ".repo").exists():
             return parent
     raise ValueError(f"{chromeos_tree_path} is not in a repo checkout")
-
-
-def GetChrootEbuildPaths(
-    chromeos_root: Union[Path, str],
-    packages: Iterable[str],
-    chroot_name: str = "chroot",
-    out_dir: str = "out",
-) -> List[str]:
-    """Gets the chroot path(s) of the package(s).
-
-    Args:
-        chromeos_root: The absolute path to the chromeos tree to use.
-        packages: A list of a package/packages to
-        be used to find their chroot path.
-        chroot_name: name of the chroot to enter.
-        out_dir: name of the out directory for the chroot.
-
-    Returns:
-        A list of chroot paths of the packages' ebuild files.
-
-    Raises:
-        ValueError: Failed to get the chroot path of a package.
-    """
-
-    chroot_paths = []
-
-    cros_sdk = [
-        "cros_sdk",
-        f"--chroot={chroot_name}",
-        f"--out-dir={out_dir}",
-    ]
-
-    # Find the chroot path for each package's ebuild.
-    for package in packages:
-        chroot_path = subprocess.check_output(
-            cros_sdk + ["--", "equery", "w", package],
-            cwd=chromeos_root,
-            encoding="utf-8",
-        )
-        chroot_paths.append(chroot_path.strip())
-
-    return chroot_paths
-
-
-def ConvertChrootPathsToAbsolutePaths(
-    chromeos_root: str,
-    chroot_paths: List[str],
-) -> List[str]:
-    """Converts the chroot path(s) to absolute symlink path(s).
-
-    Args:
-        chromeos_root: The absolute path to the chroot.
-        chroot_paths: A list of chroot paths to convert to absolute paths.
-
-    Returns:
-        A list of absolute path(s).
-
-    Raises:
-        ValueError: Invalid prefix for the chroot path or
-        invalid chroot paths were provided.
-    """
-
-    abs_paths = []
-    chroot_prefix = "/mnt/host/source/"
-    # Iterate through the chroot paths.
-    # For each chroot file path, remove '/mnt/host/source/' prefix
-    # and combine the chroot path with the result and add it to the list.
-    for chroot_path in chroot_paths:
-        if not chroot_path.startswith(chroot_prefix):
-            raise ValueError(
-                "Invalid prefix for the chroot path: %s" % chroot_path
-            )
-        rel_path = chroot_path[len(chroot_prefix) :]
-        # combine the chromeos root path + '/src/...'
-        abs_path = os.path.join(chromeos_root, rel_path)
-        abs_paths.append(abs_path)
-    return abs_paths
--- llvm_tools/chroot_test.py
+++ llvm_tools/chroot_test.py
@@ -1,67 +1,28 @@
-#!/usr/bin/env python3
 # Copyright 2020 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Unit tests for chroot helper functions."""
-
-import subprocess
-import unittest
-from unittest import mock
+"""Tests for chroot.py."""
 
 from llvm_tools import chroot
+from llvm_tools import test_helpers
 
 
-# These are unittests; protected access is OK to a point.
-# pylint: disable=protected-access
+class Test(test_helpers.TempDirTestCase):
+    """Tests for chroot.py."""
 
+    def test_chromeos_root_finding_works(self):
+        root = self.make_tempdir()
+        (root / ".repo").mkdir()
+        self.assertEqual(chroot.FindChromeOSRootAbove(root), root)
 
-class HelperFunctionsTest(unittest.TestCase):
-    """Test class for updating LLVM hashes of packages."""
+        subdir = root / "foo" / "bar" / "baz"
+        subdir.mkdir(parents=True)
+        self.assertEqual(chroot.FindChromeOSRootAbove(subdir), root)
 
-    @mock.patch.object(subprocess, "check_output")
-    def testSucceedsToGetChrootEbuildPathForPackage(self, mock_chroot_command):
-        package_chroot_path = "/chroot/path/to/package.ebuild"
-
-        # Emulate ChrootRunCommandWOutput behavior when a chroot path is found
-        # for a valid package.
-        mock_chroot_command.return_value = package_chroot_path
-
-        chroot_path = "/test/chroot/path"
-        package_list = ["new-test/package"]
-
-        self.assertEqual(
-            chroot.GetChrootEbuildPaths(chroot_path, package_list),
-            [package_chroot_path],
-        )
-
-        mock_chroot_command.assert_called_once()
-
-    def testFailedToConvertChrootPathWithInvalidPrefix(self):
-        chroot_path = "/path/to/chroot"
-        chroot_file_path = "/src/package.ebuild"
-
-        # Verify the exception is raised when a chroot path does not have the
-        # prefix '/mnt/host/source/'.
-        with self.assertRaises(ValueError) as err:
-            chroot.ConvertChrootPathsToAbsolutePaths(
-                chroot_path, [chroot_file_path]
-            )
-
-        self.assertEqual(
-            str(err.exception),
-            "Invalid prefix for the chroot path: " "%s" % chroot_file_path,
-        )
-
-    def testSucceedsToConvertChrootPathToAbsolutePath(self):
-        chroot_path = "/path/to/chroot"
-        chroot_file_paths = ["/mnt/host/source/src/package.ebuild"]
-
-        expected_abs_path = "/path/to/chroot/src/package.ebuild"
-
-        self.assertEqual(
-            chroot.ConvertChrootPathsToAbsolutePaths(
-                chroot_path, chroot_file_paths
-            ),
-            [expected_abs_path],
-        )
+    def test_chromeos_root_finding_raises_in_trivial_case(self):
+        root = self.make_tempdir()
+        subdir = root / "foo" / "bar" / "baz"
+        subdir.mkdir(parents=True)
+        with self.assertRaisesRegex(ValueError, "not in a repo checkout$"):
+            chroot.FindChromeOSRootAbove(subdir)
