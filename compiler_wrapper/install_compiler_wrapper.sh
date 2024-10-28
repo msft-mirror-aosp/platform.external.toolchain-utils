770235886e7eb47cab4b2f54b497ef66c3328a4c
mobiletc-prebuild <mobiletc-prebuild@google.com>
2024-10-28 08:00:03 +0000

    compiler_wrapper: automatic sync
    This CL automatically brings toolchain-utils' compiler_wrapper/
    directory in sync with chromiumos-overlay's. Please see
    go/crostc-repo/+/main:sync_compiler_wrapper_within_cros.sh for more info
    on this process.
    
    BUG=None
    TEST=None
    
    Change-Id: I975d1d4bec3a58e8f894019266dc0dff5113bec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/5968776
    Tested-by: mobiletc-prebuild Role Account <mobiletc-prebuild@google.com>
    Auto-Submit: mobiletc-prebuild Role Account <mobiletc-prebuild@google.com>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: George Burgess <gbiv@chromium.org>

--- compiler_wrapper/install_compiler_wrapper.sh
+++ compiler_wrapper/install_compiler_wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 #
 # Copyright 2020 The ChromiumOS Authors
 # Use of this source code is governed by a BSD-style license that can be
@@ -10,7 +10,6 @@ if [[ ! -e /etc/cros_chroot_version ]]; then
   echo "Please run this script inside chroot"
   exit 1
 fi
-set -e
 
 # Use a unique value here, since folks doing wrapper dev _likely_ want builds
 # to always be redone.
@@ -20,6 +19,53 @@ cd "$(dirname "$(readlink -m "$0")")"
 
 build_py() {
   ./build.py --version_suffix="${version_suffix}" "$@"
+}
+
+install_sysroot_wrappers() {
+  local hardness="$1"
+  local wrapper_prefix="$2"
+
+  local wrapper="${wrapper_prefix}.noccache"
+  local wrapper_ccache="${wrapper_prefix}.ccache"
+
+  echo "Updating ${hardness} wrappers: ${wrapper} and ${wrapper_ccache}"
+  build_py \
+    --config="cros.${hardness}" \
+    --use_ccache=false \
+    --use_llvm_next=false \
+    --output_file="./${wrapper}"
+  build_py \
+    --config="cros.${hardness}" \
+    --use_ccache=true \
+    --use_llvm_next=false \
+    --output_file="./${wrapper_ccache}"
+
+  # Update clang target wrappers.
+  sudo cp "./${wrapper}" "./${wrapper_ccache}" /usr/bin
+  echo "Updated clang wrapper /usr/bin/${wrapper}"
+  echo "Updated clang wrapper /usr/bin/${wrapper_ccache}"
+
+  if [[ "${hardness}" == "hardened" ]]; then
+    # Update hardened GCC target wrappers.
+    local GCC gcc_files
+    for GCC in cross-x86_64-cros-linux-gnu/gcc cross-armv7a-cros-linux-gnueabihf/gcc cross-aarch64-cros-linux-gnu/gcc; do
+      if ! gcc_files="$(equery f "${GCC}")"; then
+        if [[ $(equery l "${GCC}" 2>&1 | wc -c) -eq 0 ]]; then
+          echo "no ${GCC} package found; skipping" >&2
+          continue
+        fi
+        # Something went wrong, and the equery above probably complained about it.
+        return 1
+      fi
+      echo "Updating non-ccached ${GCC} wrapper."
+      sudo cp "./${wrapper}" "$(grep "${wrapper}" <<< "${gcc_files}")"
+      grep "${wrapper}" <<< "${gcc_files}"
+      echo "Updating ccached ${GCC} wrapper."
+      sudo cp "./${wrapper_ccache}" "$(grep "${wrapper_ccache}" <<< "${gcc_files}")"
+      grep "${wrapper_ccache}" <<< "${gcc_files}"
+    done
+  fi
+  rm -f "./${wrapper}" "./${wrapper_ccache}"
 }
 
 echo "Updated files:"
@@ -32,37 +78,5 @@ build_py \
 sudo mv ./clang_host_wrapper /usr/bin/clang_host_wrapper
 echo "/usr/bin/clang_host_wrapper"
 
-# Update the target wrappers
-build_py \
-  --config=cros.hardened \
-  --use_ccache=false \
-  --use_llvm_next=false \
-  --output_file=./sysroot_wrapper.hardened.noccache
-build_py \
-  --config=cros.hardened \
-  --use_ccache=true \
-  --use_llvm_next=false \
-  --output_file=./sysroot_wrapper.hardened.ccache
-
-# Update clang target wrappers.
-sudo cp ./sysroot_wrapper.hardened.noccache ./sysroot_wrapper.hardened.ccache /usr/bin
-echo "Updated clang wrapper /usr/bin/sysroot_wrapper.hardened.noccache"
-echo "Updated clang wrapper /usr/bin/sysroot_wrapper.hardened.ccache"
-
-# Update GCC target wrappers.
-for GCC in cross-x86_64-cros-linux-gnu/gcc cross-armv7a-cros-linux-gnueabihf/gcc cross-aarch64-cros-linux-gnu/gcc; do
-  if ! FILES="$(equery f "${GCC}")"; then
-    if [[ $(equery l "${GCC}" 2>&1 | wc -c) -eq 0 ]]; then
-      echo "no ${GCC} package found; skipping" >&2
-      continue
-    fi
-    # Something went wrong, and the equery above probably complained about it.
-    exit 1
-  fi
-  echo "Updating ${GCC} wrapper."
-  sudo cp ./sysroot_wrapper.hardened.noccache "$(grep sysroot_wrapper.hardened.noccache <<< "${FILES}")"
-  grep sysroot_wrapper.hardened.noccache <<< "${FILES}"
-  sudo cp ./sysroot_wrapper.hardened.ccache "$(grep sysroot_wrapper.hardened.ccache <<< "${FILES}")"
-  grep sysroot_wrapper.hardened.ccache <<< "${FILES}"
-done
-rm -f ./sysroot_wrapper.hardened.noccache ./sysroot_wrapper.hardened.ccache
+install_sysroot_wrappers "hardened" "sysroot_wrapper.hardened"
+install_sysroot_wrappers "nonhardened" "sysroot_wrapper"
