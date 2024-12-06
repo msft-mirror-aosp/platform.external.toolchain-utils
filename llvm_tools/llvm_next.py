60512b5a03ce9a60c0ea532c90db13f39e35186c
Jordan R Abrahams-Whitehead <ajordanr@google.com>
2024-12-06 22:33:32 +0000

    llvm_tools: Disable afdo for LLVM Next Testing
    This commit adds the autofdo disable CL to the list of
    LLVM Next CLs so we can determine if anything is broken
    beyond autofdo.
    
    BUG=b:380093598, b:368393962
    TEST=CQ
    
    Change-Id: Idfa73b749eb777d5c8ab5e8822e3f7223c9daaf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/6077457
    Reviewed-by: Bob Haarman <inglorion@chromium.org>
    Commit-Queue: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Tested-by: Jordan Abrahams-Whitehead <ajordanr@google.com>

--- llvm_tools/llvm_next.py
+++ llvm_tools/llvm_next.py
@@ -21,6 +21,7 @@ LLVM_NEXT_REV = 547379
 LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = (
     "https://chrome-internal-review.googlesource.com/c/chromeos/manifest-internal/+/7721626/1",
     "https://crrev.com/c/5924684/3",
+    "https://crrev.com/c/6077991/5",
 )
 
 # A list of CLs that constitute the current llvm-next roll.
