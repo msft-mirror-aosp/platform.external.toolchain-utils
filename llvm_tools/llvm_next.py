5f1b370a43677644e6feb63dd1b94f08418ee42e
George Burgess IV <gbiv@google.com>
2025-03-25 14:32:56 -0600

    llvm_tools: update llvm_next.py to the next version
    BUG=b:404909262
    TEST=unittests
    
    Change-Id: I635311d9c8bc845a89ef92534104e08a982047d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/toolchain-utils/+/6394598
    Tested-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Feels: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Feels: George Burgess <gbiv@chromium.org>
    Commit-Queue: Jordan Abrahams-Whitehead <ajordanr@google.com>
    Auto-Submit: George Burgess <gbiv@chromium.org>

--- llvm_tools/llvm_next.py
+++ llvm_tools/llvm_next.py
@@ -9,8 +9,8 @@ from typing import Iterable
 from llvm_tools import cros_cls
 
 
-LLVM_NEXT_HASH = "f142f8afe21bceb00fb495468aa0b5043e98c419"
-LLVM_NEXT_REV = 547379
+LLVM_NEXT_HASH = "386af4a5c64ab75eaee2448dc38f2e34a40bfed0"
+LLVM_NEXT_REV = 563880
 
 # NOTE: Always specify patch-sets for CLs. We don't want uploads by untrusted
 # users to turn into bot invocations w/ untrusted input.
@@ -18,7 +18,10 @@ LLVM_NEXT_REV = 547379
 # Please note that these are (somewhat) automatically curated. See
 # llvm_next_py_autoupdate.py.
 # pylint: disable=line-too-long
-LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = ()
+LLVM_NEXT_TESTING_CL_URLS: Iterable[str] = (
+    "crrev.com/c/6417962/1",
+    "crrev.com/i/8154895/1",
+)
 
 # A list of CLs that constitute the current llvm-next roll.
 # This is taken as the set of CLs that will be landed simultaneously in order
